53cb19abea6fa515115b5b135f32b3eea22360be
mlavin <markdlavin@gmail.com>
2012-06-03 16:46:46 -0400

    Test rabbitmq template uploads.

--- argyle/tests/test_rabbitmq.py
+++ argyle/tests/test_rabbitmq.py
@@ -35,5 +35,65 @@ class RabbitCommandsTest(RabbitTest):
         self.assertSudoCommand('rabbitmqctl set_permissions -p baz foo ".*" "^amq.gen.*$" ".*"')
 
 
+class RabbitEnvironmentTest(RabbitTest):
+    "Configuration upload for rabbitmq-env.conf."
+
+    def test_default_conf(self):
+        "Upload default rabbitmq-env.conf."
+        rabbitmq.upload_rabbitmq_environment_conf()
+        # No additional context by default
+        self.assertTemplateContext(None)
+        self.assertTemplateUsed(u'rabbitmq/rabbitmq-env.conf')
+        self.assertTemplateDesination(u'/etc/rabbitmq/rabbitmq-env.conf')
+        # RabbitMQ restarted by default
+        self.assertTrue(self.mocks['restart_service'].called)
+
+    def test_alternate_template(self):
+        "Using a different template does not change the destination."
+        rabbitmq.upload_rabbitmq_environment_conf(u'rabbit/foo.conf')
+        self.assertTemplateUsed(u'rabbit/foo.conf')
+        self.assertTemplateDesination(u'/etc/rabbitmq/rabbitmq-env.conf')
+
+    def test_additional_context(self):
+        "Additional context can be passed to the template."
+        rabbitmq.upload_rabbitmq_environment_conf(context={'foo': 'bar'})
+        self.assertTemplateContext({'foo': 'bar'})
+
+    def test_no_restart(self):
+        "Upload new configuration without a restart."
+        rabbitmq.upload_rabbitmq_environment_conf(restart=False)
+        self.assertFalse(self.mocks['restart_service'].called)
+
+
+class RabbitConfigTest(RabbitTest):
+    "Configuration upload for rabbitmq.config."
+
+    def test_default_conf(self):
+        "Upload default rabbitmq.config."
+        rabbitmq.upload_rabbitmq_conf()
+        # No additional context by default
+        self.assertTemplateContext(None)
+        self.assertTemplateUsed(u'rabbitmq/rabbitmq.config')
+        self.assertTemplateDesination(u'/etc/rabbitmq/rabbitmq.config')
+        # RabbitMQ restarted by default
+        self.assertTrue(self.mocks['restart_service'].called)
+
+    def test_alternate_template(self):
+        "Using a different template does not change the destination."
+        rabbitmq.upload_rabbitmq_conf(u'rabbit/foo.conf')
+        self.assertTemplateUsed(u'rabbit/foo.conf')
+        self.assertTemplateDesination(u'/etc/rabbitmq/rabbitmq.config')
+
+    def test_additional_context(self):
+        "Additional context can be passed to the template."
+        rabbitmq.upload_rabbitmq_conf(context={'foo': 'bar'})
+        self.assertTemplateContext({'foo': 'bar'})
+
+    def test_no_restart(self):
+        "Upload new configuration without a restart."
+        rabbitmq.upload_rabbitmq_conf(restart=False)
+        self.assertFalse(self.mocks['restart_service'].called)
+
+
 if __name__ == '__main__':
     unittest.main()
