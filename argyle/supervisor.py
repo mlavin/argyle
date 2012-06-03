35d748453e46ea07ff14036b8798d3be14ff8a3f
mlavin <markdlavin@gmail.com>
2012-06-03 18:55:15 -0400

    Tests for uploading supervisor configurations. Clean up default template contexts.

--- argyle/supervisor.py
+++ argyle/supervisor.py
@@ -13,11 +13,13 @@ def supervisor_command(command):
 @task
 def upload_supervisor_app_conf(app_name, template_name=None, context=None):
     """Upload Supervisor app configuration from a template."""
-    
-    context = context or {'app_name': app_name}
+
+    default = {'app_name': app_name}
+    context = context or {}
+    default.update(context)
     template_name = template_name or [u'supervisor/%s.conf' % app_name, u'supervisor/base.conf']
     destination = u'/etc/supervisor/conf.d/%s.conf' % app_name
-    upload_template(template_name, destination, context=context, use_sudo=True)
+    upload_template(template_name, destination, context=default, use_sudo=True)
     supervisor_command(u'update')
 
 
@@ -34,11 +36,13 @@ def remove_supervisor_app(app_name):
 @task
 def upload_celery_conf(command='celeryd', app_name=None, template_name=None, context=None):
     """Upload Supervisor configuration for a celery command."""
-    
+
     app_name = app_name or command
-    context = context or {'app_name': app_name, 'command': command}
+    default = {'app_name': app_name, 'command': command}
+    context = context or {}
+    default.update(context)
     template_name = template_name or [u'supervisor/%s.conf' % command, u'supervisor/celery.conf']
-    upload_supervisor_app_conf(app_name=app_name, template_name=template_name, context=context)
+    upload_supervisor_app_conf(app_name=app_name, template_name=template_name, context=default)
 
 
 @task
@@ -46,6 +50,8 @@ def upload_gunicorn_conf(command='gunicorn', app_name=None, template_name=None, 
     """Upload Supervisor configuration for a gunicorn server."""
     
     app_name = app_name or command
-    context = context or {'app_name': app_name, 'command': command}
+    default = {'app_name': app_name, 'command': command}
+    context = context or {}
+    default.update(context)
     template_name = template_name or [u'supervisor/%s.conf' % command, u'supervisor/gunicorn.conf']
-    upload_supervisor_app_conf(app_name=app_name, template_name=template_name, context=context)
+    upload_supervisor_app_conf(app_name=app_name, template_name=template_name, context=default)
